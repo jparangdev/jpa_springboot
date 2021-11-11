6706aa08385054078b09b2e78ea7aab3c0d2050b
jpara <jparangdev@gmail.com>
2021-11-11 23:55:31 +0900

    엔티티 생성까지 완료

---
+++ src/main/java/kr/co/jparangdev/jpa_springboot/domain/DeliveryStatus.java
@@ -0,0 +1,5 @@
+package kr.co.jparangdev.jpa_springboot.domain;
+
+public enum DeliveryStatus {
+	READY, COMP
+}
---
+++ src/main/java/kr/co/jparangdev/jpa_springboot/domain/item/Book.java
@@ -0,0 +1,18 @@
+package kr.co.jparangdev.jpa_springboot.domain.item;
+
+import javax.persistence.DiscriminatorValue;
+import javax.persistence.Entity;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@DiscriminatorValue("B")
+@Getter
+@Setter
+public class Book extends Item {
+
+	private String author;
+	private String isbn;
+
+}
